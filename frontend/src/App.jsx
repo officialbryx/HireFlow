c5989aabac631108c8d393b2527aca662147cc00
Nebiuse <biennaz47@gmail.com>
2025-02-08 14:24:54 +0800

    modified:   frontend/src/App.jsx 	modified:   frontend/src/components/NavBar.css 	new file:   frontend/src/components/SettingsPage.css 	new file:   frontend/src/components/SettingsPage.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import JobPosts from "./components/JobPost";
+import SettingsPage from "./components/SettingsPage";
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       <Routes>
         <Route path="/dashboard" element={<h2>Dashboard Page</h2>} />
         <Route path="/job-post" element={<JobPosts/>} />
-        <Route path="/settings" element={<h2>Settings Page</h2>} />
+        <Route path="/settings" element={<SettingsPage/>} />
       </Routes>
     </Router>
   );
