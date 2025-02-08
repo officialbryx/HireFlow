2f5b1ddb6ece4cee46c046a8496bf997775a141a
Nebiuse <biennaz47@gmail.com>
2025-02-08 14:12:34 +0800

    modified:   frontend/src/App.css 	modified:   frontend/src/App.jsx 	new file:   frontend/src/components/ActiveJobPosts.css 	new file:   frontend/src/components/ActiveJobPosts.jsx 	new file:   frontend/src/components/CreateJobForm.css 	new file:   frontend/src/components/CreateJobForm.jsx 	new file:   frontend/src/components/JobPost.css 	new file:   frontend/src/components/JobPost.jsx 	modified:   frontend/src/components/NavBar.css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
+import JobPosts from "./components/JobPost";
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/dashboard" element={<h2>Dashboard Page</h2>} />
-        <Route path="/job-post" element={<h2>Job Post Page</h2>} />
+        <Route path="/job-post" element={<JobPosts/>} />
         <Route path="/settings" element={<h2>Settings Page</h2>} />
       </Routes>
     </Router>
---
+++ frontend/src/components/CreateJobForm.jsx
@@ -0,0 +1,103 @@
+import React from "react";
+import "./CreateJobForm.css";
+
+const CreateJobForm = ({ formData, setFormData }) => {
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <div className="create-form">
+      <h3>Create a new job post</h3>
+
+      <div className="form-group">
+        <label>Job Title</label>
+        <input
+          type="text"
+          name="jobTitle"
+          placeholder="Enter a job title name, like 'Software Developer'"
+          value={formData.jobTitle}
+          onChange={handleChange}
+        />
+      </div>
+
+      <div className="form-group">
+        <label>Company Name</label>
+        <input
+          type="text"
+          name="companyName"
+          placeholder="Enter the company name"
+          value={formData.companyName}
+          onChange={handleChange}
+        />
+      </div>
+
+      <div className="form-row">
+        <div className="form-column">
+          <label>Responsibilities</label>
+          <textarea
+            name="responsibilities"
+            placeholder="Add responsibilities..."
+            value={formData.responsibilities}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-column">
+          <label>Qualifications</label>
+          <textarea
+            name="qualifications"
+            placeholder="Add qualifications..."
+            value={formData.qualifications}
+            onChange={handleChange}
+          />
+        </div>
+      </div>
+
+      <div className="form-group">
+        <label>Location</label>
+        <input
+          type="text"
+          name="location"
+          placeholder="Enter the location..."
+          value={formData.location}
+          onChange={handleChange}
+        />
+      </div>
+
+      <div className="form-group">
+        <label>Career</label>
+        <input
+          type="text"
+          name="workField"
+          placeholder="Enter the work field..."
+          value={formData.workField}
+          onChange={handleChange}
+        />
+      </div>
+
+      <div className="form-group">
+        <label>Type of Work</label>
+        <input
+          type="text"
+          name="typeOfWork"
+          placeholder="Enter the type of work..."
+          value={formData.typeOfWork}
+          onChange={handleChange}
+        />
+      </div>
+
+      <div className="form-group">
+        <label>Salary</label>
+        <input
+          type="text"
+          name="salary"
+          placeholder="Enter the salary for this job..."
+          value={formData.salary}
+          onChange={handleChange}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default CreateJobForm;
